a9922ac026012a7a0ddf5eccab8fa7c879b36d0c
Nikita-Antonov <antonovenikita@gmail.com>
2020-10-30 23:53:25 -0400

    Inv and Char Logic
    Began working on the Invintory Script and Charecter Logic Script

---
+++ Kiwi Kuest/Assets/Scripts/Items/Item.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : ScriptableObject
+{
+    public string itemName;
+
+    public bool isStackable;
+    private int numItem;
+
+    public enum ItemType
+    {
+        Equipment,
+        Consumable
+    }
+}
--- Kiwi Kuest/Assets/Scripts/Player/PlayerLogic.cs
+++ Kiwi Kuest/Assets/Scripts/Player/PlayerLogic.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class PlayerLogic : MonoBehaviour
 {
+
+    public int curGol = 10;
+
+    private int maxInvSpace = 40;
+    public Item[] inventory;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        inventory = new Item[maxInvSpace];
     }
 
     // Update is called once per frame
